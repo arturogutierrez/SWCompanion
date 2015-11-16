f7c08a176c9e7787ad4acd6a33b839b480f12b13
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2015-11-16 20:37:13 +0100

    Adding empty main interface with Toolbar and Drawer Layout

--- app/src/main/java/com/arturogutierrez/swcompanion/view/activity/BaseActivity.java
+++ app/src/main/java/com/arturogutierrez/swcompanion/view/activity/BaseActivity.java
@@ -1,13 +1,26 @@
 package com.arturogutierrez.swcompanion.view.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import com.arturogutierrez.swcompanion.R;
 import com.arturogutierrez.swcompanion.application.SWApplication;
 import com.arturogutierrez.swcompanion.di.component.ApplicationComponent;
 import com.arturogutierrez.swcompanion.di.module.ActivityModule;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+  @Bind(R.id.toolbar)
+  protected Toolbar toolbar;
+
+  @Bind(R.id.drawerLayout)
+  protected DrawerLayout drawerLayout;
+  protected ActionBarDrawerToggle drawerToggle;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,26 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     getApplicationComponent().inject(this);
     ButterKnife.bind(this);
+
+    configureToolbar();
+  }
+
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+
+    if (drawerToggle != null) {
+      drawerToggle.syncState();
+    }
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+
+    if (drawerToggle != null) {
+      drawerToggle.onConfigurationChanged(newConfig);
+    }
   }
 
   protected ApplicationComponent getApplicationComponent() {
@@ -28,4 +61,18 @@ public abstract class BaseActivity extends AppCompatActivity {
   }
 
   protected abstract int getLayoutResource();
+
+  private void configureToolbar() {
+    setSupportActionBar(toolbar);
+
+    showDrawerLayout();
+  }
+
+  private void showDrawerLayout() {
+    if (drawerLayout != null) {
+      drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open,
+          R.string.drawer_close);
+      drawerLayout.setDrawerListener(drawerToggle);
+    }
+  }
 }
